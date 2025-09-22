6a2be16979dd4f56b95447066ad402e9837f7ad0
RashidHamid1525 <rhamid@unomaha.edu>
2025-09-21 21:48:43 -0500

    done with lab

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,27 +1,87 @@
-#TurtleGraphics.py
-#Name:
-#Date:
-#Assignment:
+# TurtleGraphics.py
+# Name: Rashid
+# Date: 9/21/2025
+# Assignment: Turtle Graphics Assignment
 
-import turtle #needed generally but not in CodeHS
-hideturtle() #hides the default turtle in CodeHS
+
+import turtle  # Needed generally but not in CodeHS
+hideturtle()
 
 def drawSquare(myTurtle, size):
     for i in range(4):
         myTurtle.forward(size)
         myTurtle.right(90)
 
+def drawPolygon(tim, sides):
+    for s in range(sides):
+        tim.forward(50)
+        tim.right(360 / sides)
+
+def fillCorner(tom, corner):
+    drawSquare(tom, 100)
+
+    if corner == 1:
+        tom.begin_fill()
+        drawSquare(tom, 50)
+        tom.end_fill()
+
+    elif corner == 2:
+        tom.forward(50)
+        tom.begin_fill()
+        drawSquare(tom, 50)
+        tom.end_fill()
+
+    elif corner == 3:
+        tom.right(90)
+        tom.forward(50)
+        tom.left(90)
+        tom.begin_fill()
+        drawSquare(tom, 50)
+        tom.end_fill()
+
+    elif corner == 4:
+        tom.right(90)
+        tom.forward(100)
+        tom.left(90)
+        tom.forward(50)
+        tom.left(90)
+        tom.forward(50)
+        tom.right(90)
+        tom.begin_fill()
+        drawSquare(tom, 50)
+        tom.end_fill()
+
+def squaresInSquares(sally, squares):
+    sally.up()
+    sally.goto(-175, 175)
+    sally.down()
+    drawSquare(sally, 250)
+    x = 1
+    for q in range(squares - 1):
+        x += 40
+        sally.up()
+        sally.forward(20)
+        sally.right(90)
+        sally.forward(20)
+        sally.left(90)
+        sally.down()
+        drawSquare(sally, 250 - x)
 
 def main():
     myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
-    # drawPolygon(myTurtle, 8) #draws an octogon
+   
 
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
+    # Uncomment the function you want to test:
+    drawPolygon(myTurtle, 5)  # Draws a pentagon
+    # drawPolygon(myTurtle, 8)  # Draws an octagon
 
-    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
-    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
+    # fillCorner(myTurtle, 2)  # Top right corner filled
+    # fillCorner(myTurtle, 3)  # Bottom left corner filled
 
+    # squaresInSquares(myTurtle, 5)  # 5 concentric squares
+    # squaresInSquares(myTurtle, 3)  # 3 concentric squares
+
+    myTurtle.hideturtle()
+    turtle.done()
 
 main()
